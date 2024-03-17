c861d16bb5260de8ef3c60f30fec35bea1452182
Indra Mahesa <indramahesa128@gmail.com>
2024-03-17 21:29:31 +0700

    menambahkan class DataGeneric

--- kpl-tp-5/Program.cs
+++ kpl-tp-5/Program.cs
@@ -3,6 +3,8 @@
     private static void Main(string[] args)
     {
         HaloGeneric.SapaUser<String>("Praktikan");
+        var data = new DataGeneric<String>("1302220067");
+        data.PrintData();
     }
 }
 
@@ -11,5 +13,20 @@ class HaloGeneric
     public static void SapaUser<T>(T sapa)
     {
         Console.WriteLine("Halo User " + sapa);
+    }
+}
+
+class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang disimpan adalah: " + data);
     }
 }
